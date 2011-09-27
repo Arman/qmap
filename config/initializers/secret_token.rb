14792e5f2a74909dc51c076a3351cbbc41790bc2
Arman <aaerman@hotmail.com>
2011-09-27 14:00:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qmap::Application.config.secret_token = 'f5d340da5b096b5cb47851a775ac89d14b3e5b7839c43a9359884194a166988fd905e4f7217c9c8192184fac47e9fa9a9aaef923f96535967481089b2e492950'
